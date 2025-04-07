1fd903aa16533f021716ecf8df1db91b3e3347f0
KS1011-xam <kairuisun1011@gmail.com>
2025-04-07 19:30:56 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebase/config.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 将这里的值替换为你自己的 Firebase 配置
 const firebaseConfig = {
   apiKey: "AIzaSyDqAi_oZczyRWi1NcynxOh8HVWVNh-mens",
   authDomain: "argument-simulator.firebaseapp.com",
@@ -13,9 +11,11 @@ const firebaseConfig = {
   storageBucket: "argument-simulator.firebasestorage.app",
   messagingSenderId: "1073066231942",
   appId: "1:1073066231942:web:e30e2566d46b05d85b6907",
-  measurementId: "G-CMWDDQ719L"
 };
 
-// Initialize Firebase
+// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
